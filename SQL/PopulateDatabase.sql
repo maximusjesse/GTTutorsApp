74b73ce2eaf4a10911b5c3488e321e0eaaa789e2
maximusjesse <baller17@gmail.com>
2014-07-15 20:01:22 -0400

    UserTable Populated

--- SQL/PopulateDatabase.sql
+++ SQL/PopulateDatabase.sql
@@ -1,27 +1,31 @@
 Populate Database
 
 INSERT INTO User
-VALUES(333333333, 'tutor1')
-VALUES(222222222, 'stud1')
-Values(111111111, 'tutor')
-Values(888888888, 'undergrad')
-Values(987654321, 'grad')
-Values(902803967, 'junior17')
-Values(000000000, 'admin')
-Values(123456789, 'prof')
-Values(902928313, 'pink')
-Values(902000000, 'hey')
-Values(903000000, 'ok')
-Values(100000000, 'a')
-Values(200000000, 'b')
-Values(300000000, 'c')
-Values(400000000, 'd')
-Values(500000000, 'e')
-Values(600000000, 'f')
-Values(700000000, 'g')
-Values(800000000, 'h')
-Values(900000000, 'i')
-Values(101000000, 'j')
+VALUES(000000000,'admin'),
+(000000001,'undergrad1'),
+(000000002,'undergrad2'),
+(000000003,'undergrad3'),
+(000000004,'undergrad4'),
+(000000005,'undergrad5'),
+(000000006,'undergrad6'),
+(000000007,'undergrad7'),
+(000000008,'undergrad8'),
+(000000009,'undergrad9'),
+(000000010,'undergrad10'),
+(000000011,'undergrad11'),
+(000000012,'undergrad12'),
+(000000013,'undergrad13'),
+(000000014,'undergrad14'),
+(000000015,'undergrad15'),
+(000000110,'grad1'),
+(000000120,'grad2'),
+(000000130,'grad3'),
+(000000140,'grad4'),
+(000000150,'grad5'),
+(000001100,'prof1'),
+(000001200,'prof2'),
+(000001300,'prof3'),
+(000001400,'prof4')
 
 INSERT INTO Grad
 Values(987654321, 'grad@gatech.edu')
@@ -35,41 +39,41 @@ Values(111111111,111111111,4.0)
 
 INSERT INTO Course
 Values('MATH', 4305)
-Values('CS', 4400)
-Values('CS', 2316)
-Values('Math', 1000)
-Values('MATH', 2000)
-Values('Bio', 4313)
-Values('CS', 1301)
-Values('ME', 2110)
-Values('ME', 3000)
-Values('ISYE', 2027)
-Values('ISYE', 2028)
-Values('ISYE', 3232)
-Values('ISYE', 3133)
-Values('ISYE', 3039)
-Values('ISYE', 4316)
-Values('Chem', 1310)
-Values('Chem', 1211)
-Values('Chem', 1212)
-Values('Psyc', 1101)
-Values('COE', 2110)
-Values('BIO', 4000)
+('CS', 4400),
+('CS', 2316),
+('Math', 1000),
+('MATH', 2000),
+('Bio', 4313),
+('CS', 1301),
+('ME', 2110),
+('ME', 3000),
+('ISYE', 2027),
+('ISYE', 2028),
+('ISYE', 3232)
+('ISYE', 3133),
+('ISYE', 3039),
+('ISYE', 4316),
+('Chem', 1310),
+('Chem', 1211),
+('Chem', 1212),
+('Psyc', 1101),
+('COE', 2110),
+('BIO', 4000)
 
 INSERT INTO TutorTimeSlots
-Values(333333333, '09', 'Fall 2014', 'Thursday')
-Values(111111111, '10', 'Fall 2014', 'Wednesday')
-Values(333333333, '11', 'Spring 2014', 'Tuesday')
-Values(111111111,'12:00:00','Summer 2014', 'Monday')
+Values(333333333, '09', 'Fall 2014', 'Thursday'),
+(111111111, '10', 'Fall 2014', 'Wednesday'),
+(333333333, '11', 'Spring 2014', 'Tuesday'),
+(111111111,'12:00:00','Summer 2014', 'Monday')
 
 INSERT INTO Rates
 Values(902803967, 111111111, 'CS', 4400, 4, 'SO GOOD', 'Summer 2014')
 
 INSERT INTO Tutors
-Values(111111111, 'Math', 4305, 902803967)
-VALUES(111111111, 'Math', 4305, 888888888)
-Values(333333333, 'Math', 4305, 222222222)
-Values(111111111, 'CS', 4400, 902803967)
+Values(111111111, 'Math', 4305, 902803967),
+(111111111, 'Math', 4305, 888888888),
+(333333333, 'Math', 4305, 222222222),
+(111111111, 'CS', 4400, 902803967)
 
 INSERT INTO Recommends
 Values(111111111, 123456789, 4, 'VERY GOOD')
@@ -78,12 +82,11 @@ INSERT INTO Administrator
 Values(000000000)
 
 INSERT INTO Student
-Values(222222222, 'stud1@gatech.edu', 'Student 1')
-VALUES(333333333, 'tutor1@gatech.edu', 'Tutor 1')
-Values(111111111, 'tutor@gatech.edu', 'Tutor Tutorson')
-Values(888888888, 'undergrad@gatech.edu', 'Undergrad Mcgrad')
-VALUES(987654321, 'grad@gatech.edu', 'Grad McGrad')
-VALUES(902803967, 'jzhao@gatech.edu', 'Jesse Zhao')
+Values(222222222, 'stud1@gatech.edu', 'Student 1'),
+(333333333, 'tutor1@gatech.edu', 'Tutor 1'),
+(111111111, 'tutor@gatech.edu', 'Tutor Tutorson'),
+(888888888, 'undergrad@gatech.edu', 'Undergrad Mcgrad'),
+(987654321, 'grad@gatech.edu', 'Grad McGrad')
 
 INSERT INTO Professor
 VALUES(123456789)
